ed14508a1fe97cdbc220d9745ca31162edd9b2f4
Dave Willenberg <dave@detroit-english.de>
2020-04-17 16:04:35 +0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,7 @@
 const { readFileSync } = require('fs')
 const test = require('ava')
 const iptvChecker = require('./../src/index.js')
-
 const playlistPath = `${__dirname}/input/dummy.m3u`
-
 const playlistFile = readFileSync(playlistPath, {
   encoding: 'utf8',
 })
@@ -32,9 +30,18 @@ test.serial(`Should process a playlist file path`, async t => {
   t.true(resultTester(results))
 })
 
-test.serial(`Should process a playlist Buffer`, async t => {
+test.serial(`Should process a playlist data Buffer`, async t => {
   const playlistBuffer = Buffer.from(playlistFile)
   const results = await iptvChecker(playlistBuffer, {
+    timeout: 2e3,
+    parallel: 1,
+  })
+
+  t.true(resultTester(results))
+})
+
+test.serial(`Should process a playlist data string`, async t => {
+  const results = await iptvChecker(playlistFile, {
     timeout: 2e3,
     parallel: 1,
   })
@@ -50,4 +57,32 @@ test.serial(`Should throw with invalid input`, async t => {
     })
   ),
     { instanceOf: TypeError }
+})
+
+test.serial(`Should throw with invalid file path`, async t => {
+  const badPath = `${__dirname}/input/badPath.m3u`
+  await t.throwsAsync(() =>
+    iptvChecker(badPath, {
+      timeout: 2e3,
+      parallel: 1,
+    })
+  )
+})
+
+test.serial(`Should throw on URL fetch failure`, async t => {
+  await t.throwsAsync(() =>
+    iptvChecker(`https://www.google.com/teapot`, {
+      timeout: 2e3,
+      parallel: 1,
+    })
+  )
+})
+
+test.serial(`Should throw on invalid fetched input data`, async t => {
+  await t.throwsAsync(() =>
+    iptvChecker(`https://github.com`, {
+      timeout: 2e3,
+      parallel: 1,
+    })
+  )
 })
